73e2263ce3282596ab6bf4007a6f2f6c8d2bb252
Luke Busler <luke.busler@gmail.com>
2023-07-30 19:28:56 +0000

    Update icons in nav to use Spirit Island icons

--- app/components/TopNav/TopNav.jsx
+++ app/components/TopNav/TopNav.jsx
@@ -1,12 +1,11 @@
 import { Link, Stack } from "@mui/material";
-import RowingIcon from '@mui/icons-material/Rowing';
-import WhatshotIcon from '@mui/icons-material/Whatshot';
+import { toSpiritIslandText } from "~/utils/spiritIslandText";
 
 export function TopNav() {
   return (
     <Stack sx={{ justifyContent: 'space-around', alignItems: 'center' }} direction={{ xs: 'column', sm: 'row' }} spacing={1} className="root-nav">
-      <Link href="/adversary" underline="none"><RowingIcon color="primary" sx={{ fontSize: '1rem' }} /> Adversaries</Link>
-      <Link href="/spirit" underline="none"><WhatshotIcon color="primary" sx={{ fontSize: '1rem' }} /> Spirits</Link>
+      <Link href="/adversary" underline="none">{toSpiritIslandText('[[Explorer]]')} Adversaries</Link>
+      <Link href="/spirit" underline="none">{toSpiritIslandText('[[Presence]]')} Spirits</Link>
     </Stack>
   );
 }
