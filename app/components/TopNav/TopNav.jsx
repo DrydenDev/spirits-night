a322d31e443172dd789b6cc3dd44c745219e5924
Luke Busler <luke.busler@gmail.com>
2023-07-30 19:02:53 +0000

    Fix mobile styling for nav

--- app/components/TopNav/TopNav.jsx
+++ app/components/TopNav/TopNav.jsx
@@ -4,7 +4,7 @@ import WhatshotIcon from '@mui/icons-material/Whatshot';
 
 export function TopNav() {
   return (
-    <Stack sx={{ justifyContent: 'space-around' }} direction={{ xs: 'column', sm: 'row' }} className="root-nav">
+    <Stack sx={{ justifyContent: 'space-around', alignItems: 'center' }} direction={{ xs: 'column', sm: 'row' }} spacing={1} className="root-nav">
       <Link href="/adversary" underline="none"><RowingIcon color="primary" sx={{ fontSize: '1rem' }} /> Adversaries</Link>
       <Link href="/spirit" underline="none"><WhatshotIcon color="primary" sx={{ fontSize: '1rem' }} /> Spirits</Link>
     </Stack>
